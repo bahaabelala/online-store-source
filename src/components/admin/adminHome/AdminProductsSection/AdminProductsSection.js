18c85d49b953ed9f235a19ca75a9982142a30cde
bahaabelala <bahaabelala15@gmail.com>
2024-03-15 15:15:07 +0200

    handled state of no products in store in the dashboard

--- src/components/admin/adminHome/AdminProductsSection/AdminProductsSection.js
+++ src/components/admin/adminHome/AdminProductsSection/AdminProductsSection.js
@@ -2,18 +2,27 @@ import React from "react";
 import classes from "./AdminProductsSection.module.css";
 import PropTypes from "prop-types";
 import AdminProduct from "./AdminProduct/AdminProduct";
+import Alert from '../../../general/UI/Alert/Alert';
 
 const AdminProductsSection = props => {
+  console.log(props.products);
+
   return (
     <section className={classes.AdminProductsSection}>
       <h1 className={classes.title}>{props.sectionTitle}</h1>
       <div className={classes.adminProducts}>
-        {props.products.map(prod => (
-          <AdminProduct
-            key={prod.id}
-            product={prod}
-            productDeleted={props.deleteProduct} />
-        ))}
+        {props.products !== 'No data to show!' ?
+            props.products.map(prod => (
+              <AdminProduct
+                key={prod.id}
+                product={prod}
+                productDeleted={props.deleteProduct} />
+            ))
+          : (
+            <Alert>
+              There is no products in your store!
+            </Alert>
+          )}
       </div>
     </section>
   );
@@ -21,7 +30,7 @@ const AdminProductsSection = props => {
 
 AdminProductsSection.propTypes = {
   sectionTitle: PropTypes.string.isRequired,
-  products: PropTypes.array.isRequired,
+  products: PropTypes.array.isRequired || PropTypes.string.isRequired,
 };
 
 export default AdminProductsSection;
